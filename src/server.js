209240036c8dc186d6010cad6df8d74fde8b9e37
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-08-08 15:23:38 +0530

    will try to implement kafka

--- src/server.js
+++ src/server.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
-import WebSocketService from './services/webSocketService'
+import WebSocketService from './services/webSocketService.js'
+import path from 'path'
 
-
-
+const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{cors:{origin:'*'}})
 
 app.use(express.json())
-app.use('/api/chats')
-app.use('/api/message')
+app.use(express.static(path.resolve('./src/public')))
+app.use('/api/chats', (req, res) => res.send('Chats endpoint'));
+app.use('/api/message', (req, res) => res.send('Message endpoint'));
 
-WebSocketService(io)
+ WebSocketService(io)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./public/index.html'))
+});
+
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
--- src/services/webSocketService.js
+++ src/services/webSocketService.js
@@ -1,21 +1,54 @@
+const messages = {};
 
-export default async function WebSocketService(io) {
-    io.on('connection', (socket) => {
-        console.log('new socket connection..!')
+export default function WebSocketService(io) {
+  let users = {};
 
-        socket.on('joinRoom', (room,username) =>{
-            socket.join(room)
-            io.to(room).emit('message',{text:`${username} has joined!!`})
-        })
-
-        socket.on('sendMessage', async (messageData, room) => {
-            const {messageText,userId} = messageData
-            io.to(room).emit('message', {text:messageText,userId})
-            // we can save message to our DB
-        })
-
-        socket.on('disconnect', () => {
-            console.log('user left the room')
-        })
+  io.on("connection", (socket) => {
+    console.log(socket.id);
+    console.log("new socket connection..!", socket.id);
+    socket.on("register", (user_id) => {
+      users[user_id] = socket.id;
+      
+      if (messages[user_id]) {
+        messages[user_id].forEach(message => {
+            socket.emit("recieveMessage", message);
+        });
+        delete messages[user_id];
+      } {
+        
+      }
     });
-}   
+
+    socket.on("sendMessage", (messageData) => {
+      const { messageText, senderId, receiverId } = messageData;
+
+      const message = { text: messageText, senderId: senderId };
+
+      const receiverSocketId = users[receiverId];
+      try {
+        if (receiverSocketId) {
+          socket.to(receiverSocketId).emit("recieveMessage", message);
+        } else {
+          console.log("User is not online");
+          if (!messages[receiverId]) {
+            messages[receiverId] = []; // Initialize an array if not already present
+          }
+
+          messages[receiverId].push(message);
+        }
+      } catch (error) {
+        console.log(error);
+        // or we can emit an error event to the client to handle the error
+        socket.emit("error", "Failed to send message");
+      }
+    });
+
+    socket.on("disconnect", () => {
+      const userId = Object.keys(users).find((key) => users[key] === socket.id);
+      if (userId) {
+        delete users[userId];
+        console.log(`${userId} has disconnected`);
+      }
+    });
+  });
+}
